870844e145ed91325e4759fea00961e5d769ff4a
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-02-03 12:06:50 +0530

    test route to detect mood

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FaceTracker from '@/components/FaceTracker'
+const page = () => {
+  return (
+    <div>
+        <FaceTracker/>
+    </div>
+  )
+}
+
+export default page
